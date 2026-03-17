9148a2ecc1e561db9260c5e63a46b0a3af17a62a
ChandradeepMani2007 <chandradeeptripathi@gmail.com>
2026-03-17 19:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (32.79%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1) return 0;
+
+        int jumps = 0;
+        int currentEnd = 0;   // end of range we can reach with 'jumps' jumps
+        int farthest = 0;     // farthest we can reach from indices within current range
+
+        for (int i = 0; i < n - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+            // when we reached the end of the current range, we must jump
+            if (i == currentEnd) {
+                ++jumps;
+                currentEnd = farthest;
+                if (currentEnd >= n + 1) break; // already can reach end
+            }
+        }
+        return jumps;    
+    }
+};
