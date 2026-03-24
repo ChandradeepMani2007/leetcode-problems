d7e754d8e834f53926b2f03088fc4f4d8e7181ac
ChandradeepMani2007 <chandradeeptripathi@gmail.com>
2026-03-24 05:52:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (65.47%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -3,15 +3,11 @@ public:
     int bitwiseComplement(int n) {
         if (n == 0)
             return 1;
+            int mask = 1;
+            while(mask < n) {
+                mask = (mask << 1) | 1;
+            }
+            return mask ^ n;
 
-        int mask = 0;
-        int temp = n;
-
-        while (temp > 0) {
-            mask = (mask << 1) | 1;
-            temp >>= 1;
-        }
-
-        return n ^ mask;
     }
 };
