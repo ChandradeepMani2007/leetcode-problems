3c5408329613f943ea3a14f5af8ec2805170849e
ChandradeepMani2007 <chandradeeptripathi@gmail.com>
2026-04-08 01:53:54 +0530

    Time: 12 ms (70.50%) | Memory: 37.1 MB (36.25%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -3,17 +3,16 @@ public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size();
         int count = 0;
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                int is_equal = true;
-                for (int i = 0; i < n; i++) {
-                    if (grid[r][i] != grid[i][c]) {
-                        is_equal = false;
-                        break;
-                    }
-                }
-                count += is_equal;
+        map<vector<int>, int> mp;
+        for (int row = 0; row < n; row++) {
+            mp[grid[row]]++;
+        }
+        for (int c = 0; c < n; c++) {
+            vector<int> temp;
+            for (int r = 0; r < n; r++) {
+                temp.push_back(grid[r][c]);
             }
+            count += mp[temp];
         }
         return count;
     }
