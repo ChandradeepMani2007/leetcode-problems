dfa20fa3b552e2f882a7ed93115403fc9664901b
ChandradeepMani2007 <chandradeeptripathi@gmail.com>
2026-04-14 11:34:05 +0530

    Time: 7 ms (84.29%) | Memory: 43.3 MB (49.80%) - LeetSync

--- 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.cpp
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.cpp
@@ -1,31 +1,28 @@
 class SmallestInfiniteSet {
 public:
-    vector<bool> nums;
-    int i; // current smallest ko point karega
-    SmallestInfiniteSet() {
-        nums = vector<bool>(1001, true);
-        i = 1;
-    }
+    int currSmallest;
+    set<int> st;
+
+    SmallestInfiniteSet() { currSmallest = 1; }
 
     int popSmallest() {
-        int result = i;
-        nums[i] = false;
-        // i++ nahi karenge
-
-        for (int j = i + 1; j < 1001; j++) {
-            if (nums[j] == true) {
-                i = j;
-                break;
-            }
+        int result;
+        if (!st.empty()) {
+            result = *st.begin();
+            st.erase(st.begin());
+        } else {
+            result = currSmallest;
+            currSmallest += 1;
         }
+
         return result;
     }
 
     void addBack(int num) {
-        nums[num] = true;
-        if (num < i) {
-            i = num;
-        }
+        if (num >= currSmallest || st.find(num) != st.end())
+            return;
+
+        st.insert(num);
     }
 };
 
