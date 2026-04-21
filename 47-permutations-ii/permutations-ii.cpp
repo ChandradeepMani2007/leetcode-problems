17c49dbf80d2950da607c7b5be9372933c63c478
ChandradeepMani2007 <chandradeeptripathi@gmail.com>
2026-04-21 21:25:16 +0530

    Time: 7 ms (29.66%) | Memory: 15.6 MB (9.27%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>> result;
+
+    void solve(vector<int>& nums, int idx) {
+        if (idx == n) {
+            result.push_back(nums);
+
+            return;
+        }
+        unordered_set<int> uniqueSet;
+        for (int i = idx; i < n;
+             i++) { // try all possible i >= idx to swap with nums[idx]
+            if (uniqueSet.find(nums[i]) != uniqueSet.end()) {
+                continue;
+            }
+            uniqueSet.insert(nums[i]);
+            swap(nums[i], nums[idx]);
+            solve(nums, idx + 1);
+            swap(nums[i], nums[idx]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        n = nums.size();
+        solve(nums, 0);
+        return result;
+    }
+};
