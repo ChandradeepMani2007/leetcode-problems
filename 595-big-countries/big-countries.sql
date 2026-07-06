0ecfe396ef7f5a7bd97e203d0fd442ceeeada618
ChandradeepMani2007 <chandradeeptripathi@gmail.com>
2026-07-06 20:00:00 +0530

    Time: 457 ms (5.71%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -4,4 +4,11 @@ name,population,area
 FROM
 World
 WHERE
-population >= 25000000 OR area >= 3000000
+population >= 25000000 
+UNION
+SELECT 
+name,population,area
+FROM
+World
+WHERE
+area >= 3000000 
