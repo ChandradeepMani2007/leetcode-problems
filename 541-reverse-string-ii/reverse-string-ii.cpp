3bca2f3c613033a5feff7ca00b0d90429c2e448b
ChandradeepMani2007 <chandradeeptripathi@gmail.com>
2026-07-28 09:22:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (46.27%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     string reverseStr(string s, int k) {
         for (int i = 0; i < s.size(); i += 2 * k) {
-            int left = i;
-            int right = min(i + k - 1, (int)s.size() - 1);
-
-            while (left < right) {
-                swap(s[left], s[right]);
-                left++;
-                right--;
-            }
+            reverse(s.begin() + i, s.begin() + min(i + k, (int)s.size()));
         }
 
         return s;
