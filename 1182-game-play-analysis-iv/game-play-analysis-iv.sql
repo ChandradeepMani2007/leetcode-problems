ba43e3b20332508845221d4cea24d278e9e67598
ChandradeepMani2007 <chandradeeptripathi@gmail.com>
2026-08-10 11:41:44 +0530

    Time: 552 ms (80.72%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,3 +1,4 @@
+# Write your MySQL query statement
 # Write your MySQL query statement below
 SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
 FROM Activity
